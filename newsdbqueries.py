96195813e7049ba12d74c6639f530b73697850f4
verstige <39561842+verstige@users.noreply.github.com>
2018-06-15 14:08:44 +0200

    Pyscodestyle adjustments

--- newsdbqueries.py
+++ newsdbqueries.py
@@ -11,26 +11,31 @@ from prettytable import from_db_cursor
 DBNAME = "news"
 
 # Defines the list of pre-query notices
-notice = ["\nRetrieving the 3 most popular articles of all time...\n","\nSorting the authors based on popularity...\n","\nShowing the days when more than 1% of requests led to errors...\n"]
+notice = ["\nRetrieving the 3 most popular articles of all time...\n",
+          "\nSorting the authors based on popularity...\n",
+          "\nShowing the days when more than 1% of requests led to errors...\n"
+          ]
 
 # Defines the list of database queries
-query = ["select * from popnews","select * from popauthor","select * from errorcheck"]
+query = ["select * from popnews",
+         "select * from popauthor",
+         "select * from errorcheck"]
 
 
 def execute_query(query_string):
     """Connects with the database, executes the query and prints out the table.
     Input:
-		query_string(str)
-		DBNAME(str)
-	Behavior:
-		Connects with the database.
-		Creates a database cursor.
+        query_string(str)
+        DBNAME(str)
+    Behavior:
+        Connects with the database.
+        Creates a database cursor.
         Executes the database query.
-		Creates a prettytable from database result
+        Creates a prettytable from database results.
         Prints the prettytable.
         Closes the database.
-	Return:
-		none
+    Return:
+        none
     """
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
@@ -42,18 +47,18 @@ def execute_query(query_string):
 
 
 def search_db():
-    """	Loops through the queries and passes the right string on to execute_query.
-	Input:
-		query(list of strings)
-		notice(list of strings)
-	Behavior:
-		Checks if we are within the total number_of_queries.
-		Prints the corresponding notice.
+    """	Loops through queries and passes the right string on to execute_query.
+    Input:
+        query(list of strings)
+        notice(list of strings)
+    Behavior:
+        Checks if we are within the total number_of_queries.
+        Prints the corresponding notice.
         Creates the corresponding query_string variable.
-		Forwards the query_string variable to execute_query.
+        Forwards the query_string variable to execute_query.
         Update the counting variable.
-	Return:
-		none
+    Return:
+        none
     """
     number_of_queries = len(query) - 1
     query_number = 0
