0e5b8f6424e93bba62b721ce5a83b6cc87b8fd98
verstige <39561842+verstige@users.noreply.github.com>
2018-06-15 13:34:55 +0200

    Version 2

--- newsdbqueries.py
+++ newsdbqueries.py
@@ -1,64 +1,72 @@
 #!/usr/bin/env python2.7.12
-
 # "Database code" for the DB News.
 
-import psycopg2
-import bleach
 
+# Imports psycopg2 so we can connect with the databse using psql
+import psycopg2
+# Imports  from_db_cursor so we can create a table from sql
+from prettytable import from_db_cursor
+
+# Defines the database name
 DBNAME = "news"
 
-print "\nRetrieving the 3 most popular articles of all time...\n"
+# Defines the list of pre-query notices
+notice = ["\nRetrieving the 3 most popular articles of all time...\n","\nSorting the authors based on popularity...\n","\nShowing the days when more than 1% of requests led to errors...\n"]
+
+# Defines the list of database queries
+query = ["select * from popnews","select * from popauthor","select * from errorcheck"]
 
 
-def get_popnews():
-    """Return the most popular three articles of all time.
-    Sorted list with the most popular article at the top."""
+def execute_query(query_string):
+    """Connects with the database, executes the query and prints out the table.
+    Input:
+		query_string(str)
+		DBNAME(str)
+	Behavior:
+		Connects with the database.
+		Creates a database cursor.
+        Executes the database query.
+		Creates a prettytable from database result
+        Prints the prettytable.
+        Closes the database.
+	Return:
+		none
+    """
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
-    c.execute("select * from popnews")
-    data = c.fetchall()
-    print("Title, Views:")
-    for row in data:
-        print(row)
+    c.execute(query_string)
+    x = from_db_cursor(c)
+    print (x)
     db.close()
     return
 
 
-get_popnews()
-print "\nSorting the authors based on popularity...\n"
+def search_db():
+    """	Loops through the queries and passes the right string on to execute_query.
+	Input:
+		query(list of strings)
+		notice(list of strings)
+	Behavior:
+		Checks if we are within the total number_of_queries.
+		Prints the corresponding notice.
+        Creates the corresponding query_string variable.
+		Forwards the query_string variable to execute_query.
+        Update the counting variable.
+	Return:
+		none
+    """
+    number_of_queries = len(query) - 1
+    query_number = 0
+
+    while query_number <= number_of_queries:
+        print notice[query_number]
+        query_string = query[query_number]
+        execute_query(query_string)
+        query_number += 1
+    else:
+        print "\nThanks for viewing, have a great day!\n"
+        exit()
 
 
-def get_popauthor():
-    """Return the most popular authors of all time.
-    Sorted list with the most popular author at the top."""
-    db = psycopg2.connect(dbname=DBNAME)
-    c = db.cursor()
-    c.execute("select * from popauthor")
-    data = c.fetchall()
-    print("Author, Views:")
-    for row in data:
-        print(row)
-    db.close()
-    return
-
-
-get_popauthor()
-print "\nShowing the days when more than 1% of requests led to errors...\n"
-
-
-def get_errorcheck():
-    """Return all days that had more than 1% of requests lead to errors.
-    Sorted list with the most erroneous day at the top."""
-    db = psycopg2.connect(dbname=DBNAME)
-    c = db.cursor()
-    c.execute("select * from errorcheck")
-    data = c.fetchall()
-    print("Date, Status, Percent:")
-    for row in data:
-        print(row)
-    db.close()
-    return
-
-
-get_errorcheck()
-print "\nThanks for viewing, have a great day!\n"
+# Runs all the queries through the database
+print search_db()
